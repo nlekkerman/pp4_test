91a2ab5d7442a9106d513a84dd0ad2642952bd47
nikola simic <nlekkerman@gmail.com>
2024-05-09 11:17:17 +0000

    Added model for review.

--- hotel/models.py
+++ hotel/models.py
@@ -6,11 +6,13 @@ STATUS = ((0, "Draft"), (1, "Published"))
 # Create your models here.
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="hotel_reviews")
-    hotel_name = models.CharField(max_length=100)
     rating = models.IntegerField()
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ["-created_at"]
 
     def __str__(self):
-        return f"Review for {self.hotel_name} by {self.user.username}"
+        return f"Review by {self.user.username}"
